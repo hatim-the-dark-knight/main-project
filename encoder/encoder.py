b37137c2d0b82e4d5b6c76c25ab15d252314043e
Farhath Sulthana <farhathsulthana2001@gmail.com>
2023-04-24 11:51:34 +0530

    Organise

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -26,30 +26,30 @@ class ResizeImage(object):
 # tensor = resize_image(img)
 # print(tensor)
 
-class ConvEncoder(nn.Module):
+# class ConvEncoder(nn.Module):
     
-    def __init__(self, c_dim=128):
-        super().__init__()
-        self.conv0 = nn.Conv2d(3, 32, 3, stride=2)
-        self.conv1 = nn.Conv2d(32, 64, 3, stride=2)
-        self.conv2 = nn.Conv2d(64, 128, 3, stride=2)
-        self.conv3 = nn.Conv2d(128, 256, 3, stride=2)
-        self.conv4 = nn.Conv2d(256, 512, 3, stride=2)
-        self.fc_out = nn.Linear(512, c_dim)
-        self.actvn = nn.ReLU()
+#     def __init__(self, c_dim=128):
+#         super().__init__()
+#         self.conv0 = nn.Conv2d(3, 32, 3, stride=2)
+#         self.conv1 = nn.Conv2d(32, 64, 3, stride=2)
+#         self.conv2 = nn.Conv2d(64, 128, 3, stride=2)
+#         self.conv3 = nn.Conv2d(128, 256, 3, stride=2)
+#         self.conv4 = nn.Conv2d(256, 512, 3, stride=2)
+#         self.fc_out = nn.Linear(512, c_dim)
+#         self.actvn = nn.ReLU()
 
-    def forward(self, x):
-        batch_size = x.size(0)
+#     def forward(self, x):
+#         batch_size = x.size(0)
 
-        net = self.conv0(x)
-        net = self.conv1(self.actvn(net))
-        net = self.conv2(self.actvn(net))
-        net = self.conv3(self.actvn(net))
-        net = self.conv4(self.actvn(net))
-        net = net.view(batch_size, 512, -1).mean(2)
-        out = self.fc_out(self.actvn(net))
+#         net = self.conv0(x)
+#         net = self.conv1(self.actvn(net))
+#         net = self.conv2(self.actvn(net))
+#         net = self.conv3(self.actvn(net))
+#         net = self.conv4(self.actvn(net))
+#         net = net.view(batch_size, 512, -1).mean(2)
+#         out = self.fc_out(self.actvn(net))
 
-        return out
+#         return out
 
 
 class Resnet18(nn.Module):
